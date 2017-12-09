99f56d5fc459f35d94894124e944e0e70b4e8a87
Julian <julian.deocampo@gmail.com>
2017-12-10 00:51:48 +0800

    Don't rescue and print errors

--- lib/coins_ph_ruby.rb
+++ lib/coins_ph_ruby.rb
@@ -10,38 +10,26 @@ module CoinsPhRuby
       @base_url = 'https://coins.ph/api/v3'
     end
 
-    def get_crypto_accounts(currency=nil)
+    def get_crypto_accounts(_currency = nil)
       nonce = Hmac.get_nonce
       url = @base_url + '/crypto-accounts/'
-      signature = Hmac.sign_request(@api_secret, url,
-                                    nonce)
-      begin
-        response = RestClient.get(url, content_type: :json,
-                                  accept: :json,
-                                  ACCESS_SIGNATURE: signature,
-                                  ACCESS_KEY: @api_key,
-                                  ACCESS_NONCE: nonce)
-      rescue StandardError => e
-        puts e.response
-      end
-      response
+      signature = Hmac.sign_request(@api_secret, url, nonce)
+      RestClient.get(url, content_type: :json,
+                          accept: :json,
+                          ACCESS_SIGNATURE: signature,
+                          ACCESS_KEY: @api_key,
+                          ACCESS_NONCE: nonce)
     end
 
     def get_transfers(_id = nil)
       nonce = Hmac.get_nonce
       url = @base_url + '/transfers/'
-      signature = Hmac.sign_request(@api_secret, url,
-                                    nonce)
-      begin
-        response = RestClient.get(url, content_type: :json,
-                                       accept: :json,
-                                       ACCESS_SIGNATURE: signature,
-                                       ACCESS_KEY: @api_key,
-                                       ACCESS_NONCE: nonce)
-      rescue StandardError => e
-        puts e.response
-      end
-      response
+      signature = Hmac.sign_request(@api_secret, url, nonce)
+      RestClient.get(url, content_type: :json,
+                          accept: :json,
+                          ACCESS_SIGNATURE: signature,
+                          ACCESS_KEY: @api_key,
+                          ACCESS_NONCE: nonce)
     end
 
     def transfer(amount, account, target_address, message = nil)
@@ -50,25 +38,20 @@ module CoinsPhRuby
       url = @base_url + '/transfers/'
       signature = Hmac.sign_request(@api_secret, url,
                                     nonce, body)
-      begin
-        response = RestClient.post(url, body, content_type: :json,
-                                              accept: :json,
-                                              ACCESS_SIGNATURE: signature,
-                                              ACCESS_KEY: @api_key,
-                                              ACCESS_NONCE: nonce)
-      rescue StandardError => e
-        puts e.response
-      end
-      response
+      RestClient.post(url, body, content_type: :json,
+                                 accept: :json,
+                                 ACCESS_SIGNATURE: signature,
+                                 ACCESS_KEY: @api_key,
+                                 ACCESS_NONCE: nonce)
     end
 
     private
 
     def build_transfer_body(account, amount, message, target_address)
       body = {
-          target_address: target_address,
-          account: account,
-          amount: amount
+        target_address: target_address,
+        account: account,
+        amount: amount
       }
       body[:message] = message unless message.nil?
       body.to_json.gsub(/\s+/, '')
--- lib/coins_ph_ruby/version.rb
+++ lib/coins_ph_ruby/version.rb
@@ -1,3 +1,3 @@
 module CoinsPhRuby
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
 end
